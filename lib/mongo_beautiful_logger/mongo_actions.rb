6ca720af59a89c0847530e9cf1c0120dce1e4e8e
Javier Menéndez Rizo <jmenendez087@gmail.com>
2025-11-07 22:55:52 -0500

    Ruby 3.4 support (#1)
    Add support for ruby 3.4.x. The problem is that ruby 3.4 changed the behavior of `Hash#inspect` adding spaces between `=>`
    
    References:
    - https://www.ruby-lang.org/en/news/2024/12/25/ruby-3-4-0-released/
    - https://rubyreferences.github.io/rubychanges/3.4.html#inspect-rendering-have-been-changed
    - http://bugs.ruby-lang.org/issues/20433

--- lib/mongo_beautiful_logger/mongo_actions.rb
+++ lib/mongo_beautiful_logger/mongo_actions.rb
@@ -3,24 +3,27 @@ require "mongo_beautiful_logger/colors"
 module MongoActions
   include Colors
 
+  # https://bugs.ruby-lang.org/issues/20433
+  arrow = (RUBY_VERSION >= '3.4') ? ' =>' : '=>'
+
   # substring matches and the corresponding colors for mongodb actions
-  FIND         = { match: "\"find\"=>",        color: BLUE }
-  UPDATE       = { match: "\"update\"=>",      color: YELLOW }
-  INSERT       = { match: "\"insert\"=>",      color: GREEN }
-  DELETE       = { match: "\"delete\"=>",      color: RED }
-  AGGREGATE    = { match: "\"aggregate\"=>",   color: MAGENTA }
-  SUCCEEDED    = { match: "succeeded",         color: GREEN }
-  FAILED       = { match: "failed",            color: RED }
-  ERROR        = { match: "error",             color: RED }
-  ENDSESSION   = { match: "\"endsessions\"=>", color: YELLOW }
-  INITIALIZING = { match: "initializing",      color: GREEN }
+  FIND         = { match: "\"find\"#{arrow}",        color: BLUE }
+  UPDATE       = { match: "\"update\"#{arrow}",      color: YELLOW }
+  INSERT       = { match: "\"insert\"#{arrow}",      color: GREEN }
+  DELETE       = { match: "\"delete\"#{arrow}",      color: RED }
+  AGGREGATE    = { match: "\"aggregate\"#{arrow}",   color: MAGENTA }
+  SUCCEEDED    = { match: "succeeded",               color: GREEN }
+  FAILED       = { match: "failed",                  color: RED }
+  ERROR        = { match: "error",                   color: RED }
+  ENDSESSION   = { match: "\"endsessions\"#{arrow}", color: YELLOW }
+  INITIALIZING = { match: "initializing",            color: GREEN }
   ACTIONS      = [ FIND, UPDATE, INSERT, DELETE, AGGREGATE, 
                    SUCCEEDED, FAILED, ERROR, ENDSESSION, INITIALIZING ]
   
   # substring matches for unnecessary log messages that will be filtered out
   UNNECESSARY  = ["opology", "server description"]
 
-  # regex for for the log prefix that will be filtered out
+  # regex for the log prefix that will be filtered out
   # matches: +| localhost:27017 | app_test.update | STARTED |+
   # note: +| STARTED |+ and +| SUCCEEDED |+ will be filtered due to redundancy,
   # but +| FAILED |+ and others will not
